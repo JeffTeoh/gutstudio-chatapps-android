a4d5ae179104ef3ce542779fb59d0ddc4a8fe186
JeffTeoh <jeffrey@gutstudio.com>
2018-02-22 18:44:48 +0800

    If applied, this commit will add basic UI for each tabs
    - Change the name for each tabs

---
+++ app/src/main/java/com/example/android/chatapps_android/Tab2Chat.java
@@ -0,0 +1,21 @@
+package com.example.android.chatapps_android;
+
+/**
+ * Created by Jeffrey on 2/22/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Chat extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2chat, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/android/chatapps_android/chatbox.java
+++ app/src/main/java/com/example/android/chatapps_android/chatbox.java
@@ -2,8 +2,6 @@ package com.example.android.chatapps_android;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -90,41 +88,7 @@ public class chatbox extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_chatbox, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
+    //deleted PlaceholderFragment class from here
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
@@ -137,9 +101,17 @@ public class chatbox extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //Returning the current tabs
+            switch (position) {
+                case 0:
+                    Tab1Contacts tab1 = new Tab1Contacts();
+                    return tab1;
+                case 1:
+                    Tab2Chat tab2 = new Tab2Chat();
+                    return tab2;
+                default:
+                    return null;
+            }
         }
 
         @Override
