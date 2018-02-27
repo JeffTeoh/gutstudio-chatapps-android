6002cd11af7ea1656f52c768acf4f52c4a49e675
JeffTeoh <jeffrey@gutstudio.com>
2018-02-28 01:57:04 +0800

    Enable item in card view can be clicked

--- app/src/main/java/com/example/android/chatapps_android/HomeFragment.java
+++ app/src/main/java/com/example/android/chatapps_android/HomeFragment.java
@@ -2,11 +2,15 @@ package com.example.android.chatapps_android;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,11 +20,11 @@ import android.widget.Toast;
  */
 public class HomeFragment extends Fragment {
 
+    GridLayout mainGrid;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +32,10 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
 
+        mainGrid = (GridLayout) v.findViewById(R.id.mainGrid);
 
+        //set Event
+        setSingleEvent (mainGrid);
 
         final SwipeRefreshLayout mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.fragment_home);
 
@@ -43,6 +50,21 @@ public class HomeFragment extends Fragment {
         );
 
         return v;
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for(int i=0; i<mainGrid.getChildCount(); i++)
+        {
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText (getContext(), "Clicked at activity " + finalI, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 }
